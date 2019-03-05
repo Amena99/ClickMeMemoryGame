3e7ab3df3ff69e54c680e0ebca6f88c1f81fceb9
Amena Ahmed <am.ahmed09@gmail.com>
2019-03-04 22:03:50 -0600

    get rid of node modules

---
+++ client/build/precache-manifest.03c22f8b174d7ba31485044156518d3d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "72302d60ed4292dda4f1",
+    "url": "/static/js/main.72302d60.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "91625470139d10de214a706e1632029f",
+    "url": "/index.html"
+  }
+];
